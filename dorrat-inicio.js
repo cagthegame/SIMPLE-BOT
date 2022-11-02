aff0a02eb9f81968a1dff4f623d836277146e6a7
KantuBot <113566411+cagthegame@users.noreply.github.com>
2022-11-02 14:24:30 -0600

    Update dorrat-inicio.js

--- dorrat-inicio.js
+++ dorrat-inicio.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('DORRAT-BOT-MD', {
+say('KANTU-BOT-MD', {
 font: 'tiny',
 align: 'center',
 colors: ['#ff8000']})
-say(`CREADO POR DIEGO-OFC`, {
+say(`CREADO POR CRISs-GC`, {
 font: 'console',
 align: 'center',
 colors: ['red']})
