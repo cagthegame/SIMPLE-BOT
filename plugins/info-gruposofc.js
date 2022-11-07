6e59ac2ef6144981c663bd3a28a4aa5cd955dc84
KantuBot <113566411+cagthegame@users.noreply.github.com>
2022-11-06 18:19:22 -0600

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,19 +1,20 @@
 let media = './galería/grupos-oficiales.jpg'
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
-*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 †𝐃⃟𝕺𝐑⃯𝐑𝐇⃯𝚵𝐓᪣𝕭⃯𝚹⃯𝐓⃤ 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
+*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝐓𝐄 𝐈𝐍𝐕𝐈𝐓𝐎 𝐀 𝐔𝐍𝐈𝐑𝐓𝐄 𝐀 𝐌𝐈𝐒 𝐆𝐑𝐔𝐏𝐎𝐒 𝐎𝐅𝐈𝐂𝐈𝐀𝐋𝐄𝐒 𝐏𝐀𝐑𝐀 𝐂𝐎𝐍𝐕𝐈𝐕𝐈𝐑 𝐂𝐎𝐍 𝐋𝐀 𝐂𝐎𝐌𝐔𝐍𝐈𝐃𝐀𝐃 :𝐃
+*
 
 *➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝙱𝚘𝚝:*
-*1.-* https://chat.whatsapp.com/LlAbJM659dOBVJnxOIMl9h
+*1.-* https://chat.whatsapp.com/LLWPIF1EGkqBDgCjALubV5
 
-*2._* https://chat.whatsapp.com/FxlQJ4A3tOjGx2Qu3BITfb
+*2._* https://chat.whatsapp.com/LLWPIF1EGkqBDgCjALubV5
 
-*3._* https://chat.whatsapp.com/JjO8d79Y6x85Lb9AVJTacg
+*3._* https://chat.whatsapp.com/LLWPIF1EGkqBDgCjALubV5
 
-*4._* https://chat.whatsapp.com/GzSa1szOscjCZS4jBPBvtG
+*4._* https://chat.whatsapp.com/LLWPIF1EGkqBDgCjALubV5
 
-*5._* https://chat.whatsapp.com/KMOHXtuV8A594uS8fVq0v9
+*5._* https://chat.whatsapp.com/IrvY5IpNHrOC92HUBvlCEH
 
-*6._* https://chat.whatsapp.com/LEMnxt8sApu9HeBbpxI50w
+*6._* https://chat.whatsapp.com/IrvY5IpNHrOC92HUBvlCEH
 
 `.trim(), wm, media, [['💟 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 💟', '#menu']], m)
 handler.command = /^linkgc|grupos$/i
