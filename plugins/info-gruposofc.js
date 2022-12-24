9d07c5b16e4718c62937009ccf6b9f14f1510c87
KantuBot <113566411+cagthegame@users.noreply.github.com>
2022-12-23 19:40:47 -0600

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,21 +1,24 @@
-let media = './galería/grupos-oficiales.jpg'
+let media = './Menu2.jpg'
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
-*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝐓𝐄 𝐈𝐍𝐕𝐈𝐓𝐎 𝐀 𝐔𝐍𝐈𝐑𝐓𝐄 𝐀 𝐌𝐈𝐒 𝐆𝐑𝐔𝐏𝐎𝐒 𝐎𝐅𝐈𝐂𝐈𝐀𝐋𝐄𝐒 𝐏𝐀𝐑𝐀 𝐂𝐎𝐍𝐕𝐈𝐕𝐈𝐑 𝐂𝐎𝐍 𝐋𝐀 𝐂𝐎𝐌𝐔𝐍𝐈𝐃𝐀𝐃 :𝐃
-*
-
-*➤ 𝙶𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝙱𝚘𝚝:*
-*1.-* https://chat.whatsapp.com/IrvY5IpNHrOC92HUBvlCEH
-
-*2._* https://chat.whatsapp.com/FPkQqMHIMLh2gR626AOU7V
-
-*3._* https://chat.whatsapp.com/IrvY5IpNHrOC92HUBvlCEH
-
-*4._* https://chat.whatsapp.com/FPkQqMHIMLh2gR626AOU7V
-
-*5._* https://chat.whatsapp.com/IrvY5IpNHrOC92HUBvlCEH
-
-*6._* https://chat.whatsapp.com/FPkQqMHIMLh2gR626AOU7V
-
+*𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 ™𝐊𝐀𝐍𝐓𝐔-𝐁𝐎𝐓 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
+╭━━❍𝐊𝐀𝐍𝐓𝐔-𝐁𝐎𝐓 𝐁𝐲 𝐂𝐑𝐈𝐒❍━━╮
+┃ ╭━━━━━━━━━━━━━━━━╮
+┃ ┃ ╭┈────────────╮
+┃ ┃ │❍ 𝐆𝐑𝐔𝐏𝐎𝐒-𝐎𝐅𝐈𝐂𝐈𝐀𝐋𝐄𝐒 ❍
+┃ ┃ ╰┈────────────╯
+┃ ╰━━━━━━━━━━━━━━━━╯
+╰━━━𝐆𝐑𝐔𝐏𝐎𝐒-𝐎𝐅𝐈𝐂𝐈𝐀𝐋𝐄𝐒╾━━━╯
+╭┈───────────────╮
+│ ▻ 𝙶𝚁𝚄𝙿𝙾 𝙳𝙴 𝙰𝙲𝚃𝚄𝙰𝙻𝙸𝚉𝙰𝙲𝙸𝙾𝙽𝙴𝚂 ◅
+╰┈───────────────╯
+╭━━━━━━━━━━━━━━━━━━━
+┃⇛ https://chat.whatsapp.com/FPkQqMHIMLh2gR626AOU7V ⇚
+╰━━━━━━━━━━━━━━━━━━━╯
+╭━━━━━━━━━━━━━━━━━━━╾•
+┃1: https://chat.whatsapp.com/Kzb29eA53Td0dLHTxSrmb7 ⇚
+┃2: https://chat.whatsapp.com/Kzb29eA53Td0dLHTxSrmb7 ⇚
+┃3: https://chat.whatsapp.com/Kzb29eA53Td0dLHTxSrmb7 ⇚
+╰━━━━━━━━━━━━━━━━━━━━━╯
 `.trim(), wm, media, [['💟 𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻 💟', '#menu']], m)
 handler.command = /^linkgc|grupos$/i
 export default handler
