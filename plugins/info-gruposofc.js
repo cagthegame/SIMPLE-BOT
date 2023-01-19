59ad6fdbfaff3c43d24ce0679e2c7c98ce38960f
CRIS OFC <113566411+cagthegame@users.noreply.github.com>
2023-01-18 19:29:58 -0600

    Update info-gruposofc.js

--- plugins/info-gruposofc.js
+++ plugins/info-gruposofc.js
@@ -1,4 +1,4 @@
-let media = './Menu2.jpg'
+let media = './grupos-oficiales.jpg'
 let handler = async (m, { conn, command }) => conn.sendButton(m.chat, `
 *𝙷𝙾𝙻𝙰 𝚄𝚂𝚄𝙰𝚁𝙸𝙾 👋🏻, 𝚃𝙴 𝙸𝙽𝚅𝙸𝚃𝙾 𝙰 𝚄𝙽𝙸𝚁𝚃𝙴 𝙰 𝙻𝙾𝚂 𝙶𝚁𝚄𝙿𝙾𝚂 𝙾𝙵𝙸𝙲𝙸𝙰𝙻𝙴𝚂 𝙳𝙴 ™𝐊𝐀𝐍𝐓𝐔-𝐁𝐎𝐓 𝙿𝙰𝚁𝙰 𝙲𝙾𝙽𝚅𝙸𝚅𝙸𝚁 𝙲𝙾𝙽 𝙻𝙰 𝙲𝙾𝙼𝚄𝙽𝙸𝙳𝙰𝙳 :D*
 ╭━━❍𝐊𝐀𝐍𝐓𝐔-𝐁𝐎𝐓 𝐁𝐲 𝐂𝐑𝐈𝐒❍━━╮
