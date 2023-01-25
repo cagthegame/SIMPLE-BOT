8f3baa4b9ef716627fe12c3300b5276f1c6f3eda
CRIS OFC <113566411+cagthegame@users.noreply.github.com>
2023-01-24 19:35:15 -0600

    Update menu-lista.js

--- plugins/menu-lista.js
+++ plugins/menu-lista.js
@@ -4,6 +4,8 @@ const { levelling } = '../lib/levelling.js'
 import moment from 'moment-timezone'
 let handler = async (m, { conn, usedPrefix, command, args, isOwner, isAdmin, isROwner }) => {
 let locale = 'es'
+let fkontak = { "key": { "participants":"0@s.whatsapp.net", "remoteJid": "status@broadcast", "fromMe": false, "id": "Halo" }, "message": { "contactMessage": { "vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD` }}, "participant": "0@s.whatsapp.net" }
+let taguser = await conn.getName(m.sender)
 let d = new Date(new Date + 3600000)
 let time = d.toLocaleTimeString(locale, {
       hour: 'numeric',
@@ -19,57 +21,61 @@ let _uptime = process.uptime() * 1000
 let uptime = clockString(_uptime) 
 wm = global.wm
 vs = global.vs
+
+let pareja = global.db.data.users[m.sender].pasangan
 let { exp, limit, dorracoins, level, role } = global.db.data.users[m.sender]
 let { min, xp, max } = xpRange(level, global.multiplier)
 //let name = await conn.getName(m.sender)
 
 const sections = [{
-title: comienzo + ' 🔰 𝐼𝑁𝐹𝑂 𝐾𝐴𝑁𝑇𝑈 - 𝐵𝑂𝑇 🔰 ' + fin,
+title: comienzo + ' ' + lenguajeGB['smsLista1']() + ' ' + fin,
 rows: [
-{title: "│🤴│𝐍𝐔𝐌𝐄𝐑𝐎 𝐃𝐄 𝐌𝐈 𝐂𝐑𝐄𝐀𝐃𝐎𝐑", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚘𝚜 𝚗ú𝚖𝚎𝚛𝚘𝚜 𝚍𝚎 𝚖𝚒 𝚌𝚛𝚎𝚊𝚍𝚘𝚛", rowId: `${usedPrefix}creator`},  
-{title: "│🏓│𝐈𝐍𝐅𝐎𝐁𝐎𝐓", description: "m𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚒𝚗𝚏𝚘𝚋𝚘𝚝", rowId: `${usedPrefix}infobot`},
-{title: "│☄️│𝐌𝐄𝐍𝐔 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐎", description: "𝙼𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚊 𝚕𝚒𝚜𝚝𝚊 𝚍𝚎 𝚖𝚎𝚗𝚞 𝚌𝚘𝚖𝚙𝚕𝚎𝚝𝚘 de KANTU-BOT", rowId: `${usedPrefix}allmenu`},
-{title: "│🤖│𝐆𝐑𝐔𝐏𝐎 𝐎𝐅𝐈𝐂𝐈𝐀𝐋 𝐃𝐄𝐋 𝐁𝐎𝐓", description: "𝚖𝚞𝚎𝚜𝚝𝚛𝚊 𝚕𝚘𝚜 𝚐𝚛𝚞𝚙𝚘𝚜 𝚘𝚏𝚒𝚌𝚒𝚊𝚕𝚎𝚜 𝚍𝚎𝚕 𝚋𝚘𝚝", rowId: `${usedPrefix}grupos`},
-{title: "│🖥️│𝐒𝐄𝐑 𝐒𝐔𝐁-𝐁𝐎𝐓", description: "𝙹𝙰𝙳𝙸𝙱𝙾𝚃 𝙼𝚄𝙻𝚃𝙸𝙳𝙸𝚅𝙸𝙲𝙴", rowId: `${usedPrefix}jadibot`},
-{title: "│🔰│𝐈𝐍𝐒𝐓𝐀𝐋𝐀𝐑-𝐊𝐀𝐍𝐓𝐔-𝐁𝐎𝐓", description: "INFORMACIÓN PARA INSTALAR A KANTU-BOT", rowId: `${usedPrefix}instalarbot`}
+{title: "│🌐│MINECRAFT-SERVER", rowId:`${usedPrefix}mc-server`},  
+{title: lenguajeGB['smsLista2'](), rowId:`${usedPrefix}creator`},  
+{title: lenguajeGB['smsLista3'](), rowId:`${usedPrefix}infobot`},
+{title: lenguajeGB['smsDonar'](), rowId:`${usedPrefix}donar`},
+{title: lenguajeGB['smsLista4'](), rowId:`${usedPrefix}allmenu`},
+{title: lenguajeGB['smsLista5'](), rowId:`${usedPrefix}grupos`},
+{title: lenguajeGB['smsLista6'](), rowId:`${usedPrefix}jadibot`},
+{title: lenguajeGB['smsLista7'](), rowId:`${usedPrefix}instalarbot`}
 
 ]},{
-title: comienzo + ' 💲 𝐌𝐄𝐍𝐔 𝐄𝐂𝐎𝐍𝐎𝐌𝐈𝐀 💲' + fin,
+title: comienzo + ' ' + lenguajeGB['smsLista8']() + ' ' + fin,
 rows: [
-{title: "🏆 𝐓𝐀𝐁𝐋𝐀 𝐂𝐋𝐀𝐒𝐈𝐅𝐈𝐂𝐀𝐓𝐎𝐑𝐈𝐀", rowId: `${usedPrefix}lb`},
-{title: "⛏️ 𝐌𝐈𝐍𝐀𝐑", rowId: `${usedPrefix}minar`},
-{title: "👨‍🏭 𝐓𝐑𝐀𝐁𝐀𝐉𝐀𝐑", rowId: `${usedPrefix}work`},
-{title: "👜 𝐁𝐀𝐋𝐀𝐍𝐂𝐄", rowId: `${usedPrefix}bal`},
-{title: "💰 𝐌𝐄𝐍𝐔 𝐑𝐏𝐆", rowId: `${usedPrefix}rpgmenu`},
-{title: "💎 𝐂𝐎𝐌𝐏𝐑𝐀𝐑 𝐃𝐈𝐀𝐌𝐀𝐍𝐓𝐄𝐒", rowId: `${usedPrefix}buyall`}
+{title: lenguajeGB['smsLista9'](),  rowId: `${usedPrefix}lb`},
+{title: lenguajeGB['smsLista10'](), rowId: `${usedPrefix}minar`},
+{title: lenguajeGB['smsLista11'](), rowId: `${usedPrefix}work`},
+{title: lenguajeGB['smsLista12'](), rowId: `${usedPrefix}inventory`},
+{title: lenguajeGB['smsLista13'](), rowId: `${usedPrefix}rpgmenu`},
+{title: lenguajeGB['smsLista14'](), rowId:  `${usedPrefix}buy limit 1`}
 ]},{	
-title: comienzo + ' 🌟 𝐌𝐄𝐍𝐔𝐒 ' + fin,
+title: comienzo + ' ' + lenguajeGB['smsLista15']() + ' ' + fin,
 rows: [
-{title: "📦 𝐌𝐄𝐍𝐔 𝐃𝐄 𝐃𝐄𝐒𝐂𝐀𝐑𝐆𝐀𝐒", rowId: `${usedPrefix}descargasmenu`},
-{title: "🎧 𝐌𝐄𝐍𝐔 𝐀𝐔𝐃𝐈𝐎𝐒", rowId: `${usedPrefix}audios`},  
-{title: "🎮 𝐌𝐄𝐍𝐔 𝐉𝐔𝐄𝐆𝐎𝐒", rowId: `${usedPrefix}juegosdelgrupo`},
- {title: "🔰 𝐌𝐄𝐍𝐔 𝐒𝐓𝐈𝐂𝐊𝐄𝐑𝐒", rowId: `${usedPrefix}stickermenu`},
- {title: "🏛 𝐌𝐄𝐍𝐔 𝐀𝐍𝐈𝐌𝐄", rowId: `${usedPrefix}animemenu`},
- {title: "👑 𝐌𝐄𝐍𝐔 𝐎𝐖𝐍𝐄𝐑", rowId: `${usedPrefix}soloparaelpropietario`},  
-{title: "🔞 𝐌𝐄𝐍𝐔 +18", rowId: `${usedPrefix}hornymenu`}
+{title: lenguajeGB['smsLista16'](), rowId: `${usedPrefix}descargasmenu`},
+{title: lenguajeGB['smsLista17'](), rowId:`${usedPrefix}audios`},  
+{title: lenguajeGB['smsLista18'](), rowId: `${usedPrefix}juegosdelgrupo`},
+{title: lenguajeGB['smsLista19'](), rowId: `${usedPrefix}stickermenu`},
+{title: lenguajeGB['smsLista20'](), rowId:`${usedPrefix}animemenu`},
+{title: lenguajeGB['smsLista21'](), rowId:`${usedPrefix}soloparaelpropietario`},  
+{title: lenguajeGB['smsLista22'](), rowId: `${usedPrefix}hornymenu`}
 ]},{	
-title: comienzo + '🗂️ 𝐄𝐍𝐀𝐁𝐋𝐄/𝐃𝐈𝐒𝐀𝐁𝐋𝐄 ' + fin,
+title: comienzo + '🗂️ ENABLE/DISABLE ' + fin,
 rows: [
-{title: "🕋 𝐄𝐍𝐀𝐁𝐋𝐄 𝐌𝐄𝐍𝐔 𝐂𝐎𝐌𝐏𝐋𝐄𝐓𝐎", rowId: `${usedPrefix}enable`},  
-{title: "✨ 𝐄𝐍𝐀𝐁𝐋𝐄 𝐖𝐄𝐋𝐂𝐎𝐌𝐄", rowId: `${usedPrefix}enable welcome`},
- {title: "✨ 𝐃𝐈𝐒𝐀𝐁𝐋𝐄 𝐖𝐄𝐋𝐂𝐎𝐌𝐄 ", rowId: `${usedPrefix}disable welcome`},
-{title: "🔗 𝐄𝐍𝐀𝐁𝐋𝐄 𝐀𝐍𝐓𝐈𝐋𝐈𝐍𝐊", rowId: `${usedPrefix}enable antilink`},  
- {title: "🔗 𝐃𝐈𝐒𝐀𝐁𝐋𝐄 𝐀𝐍𝐓𝐈𝐋𝐈𝐍𝐊", rowId: `${usedPrefix}disable antilink`},
-{title: "🧬 𝐄𝐍𝐀𝐁𝐋𝐄 𝐀𝐍𝐓𝐈𝐀𝐑𝐀𝐁𝐄𝐒", rowId: `${usedPrefix}enable antiarabes`},    
-{title: "🧬 𝐃𝐈𝐒𝐀𝐁𝐋𝐄 𝐀𝐍𝐓𝐈𝐀𝐑𝐀𝐁𝐄𝐒", rowId: `${usedPrefix}disable antiarabes`},
- {title: "🥵 𝐄𝐍𝐀𝐁𝐋𝐄 𝐌𝐎𝐃𝐎𝐇𝐎𝐑𝐍𝐘", rowId: `${usedPrefix}enable modohorny`},
- {title: "🥵 𝐃𝐈𝐒𝐀𝐁𝐋𝐄 𝐌𝐎𝐃𝐎𝐇𝐎𝐑𝐍𝐘", rowId: `${usedPrefix}disable modohorny`},
-{title: "⛔ 𝐄𝐍𝐀𝐁𝐋𝐄 𝐑𝐄𝐒𝐓𝐑𝐈𝐂𝐓", rowId: `${usedPrefix}enable restrict`},
-{title: "⛔ 𝐃𝐈𝐒𝐀𝐁𝐋𝐄 𝐑𝐄𝐒𝐓𝐑𝐈𝐂𝐓", rowId: `${usedPrefix}disable restrict`},
- {title: "📵 𝐄𝐍𝐀𝐁𝐋𝐄 𝐀𝐍𝐓𝐈𝐋𝐋𝐀𝐌𝐀𝐃𝐀", rowId: `${usedPrefix}enable anticall`},
- {title: "📵 𝐃𝐈𝐒𝐀𝐁𝐋𝐄 𝐀𝐍𝐓𝐈𝐋𝐋𝐀𝐌𝐀𝐃𝐀", rowId: `${usedPrefix}disable anticall`},   
-{title: "🎵 𝐄𝐍𝐀𝐁𝐋𝐄 𝐀𝐔𝐃𝐈𝐎𝐒", rowId: `${usedPrefix}enable audios`},
-{title: "🎵 𝐃𝐈𝐒𝐀𝐁𝐋𝐄 𝐀𝐔𝐃𝐈𝐎𝐒", rowId: `${usedPrefix}disable audios`}
+{title: "│🕋│ENABLE MENU COMPLETO", rowId: `${usedPrefix}enable`},  
+{title: "│✨│ENABLE WELCOME", rowId: `${usedPrefix}enable welcome`},
+{title: "│✨│DISABLE WELCOME", rowId: `${usedPrefix}disable welcome`},
+{title: "│🔗│ENABLE ANTILINK", rowId: `${usedPrefix}enable antilink`},  
+{title: "│🔗│DISABLE ANTILINK", rowId: `${usedPrefix}disable antilink`},
+{title: "│🧬│ENABLE ANTIARABES", rowId: `${usedPrefix}enable antiarabes`},    
+{title: "│🧬│DISABLE ANTIARABES", rowId: `${usedPrefix}disable antiarabes`},
+{title: "│🥵│ENABLE MODOHORNY", rowId: `${usedPrefix}enable modohorny`},
+{title: "│🥵│DISABLE MODOHORNY", rowId: `${usedPrefix}disable modohorny`},
+{title: "│⛔│ENABLE RESTRICT", rowId: `${usedPrefix}enable restrict`},
+{title: "│⛔│DISABLE RESTRICT", rowId: `${usedPrefix}disable restrict`},
+{title: "│📵│ENABLE ANTILLAMADA", rowId: `${usedPrefix}enable anticall`},
+{title: "│📵│DISABLE ANTILLAMADA", rowId: `${usedPrefix}disable anticall`},   
+{title: "│🎵│ENABLE AUDIOS", rowId: `${usedPrefix}enable audios`},
+{title: "│🎵│DISABLE AUDIOS", rowId: `${usedPrefix}disable audios`}
 ]}]
 
 
@@ -83,33 +89,38 @@ let name = await conn.getName(m.sender)
 const listMessage = {
       
 text: `
-┏─────────────────────⬣\n│${ucapan()}\n│💚•.¸💚¸.• *${name}* •.¸💚¸.•💚\n┗───────────────────── ⳹
-
-╔═══〔 *${wm}* 〕═══⬣
-║ ⏱️ ▢ *Hora*    
-║ ⌚ ▢ ${time}   
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ ⚡ ▢ *Activo durante*
-║ ⚡ ▢ ${uptime}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ 💎 ▢ *Versión de 𝐊𝐀𝐍𝐓𝐔 - 𝐁𝐎𝐓*
-║ 💎 ▢ ${vs}
-║ 🔳 ▢ Library:
-║ 🖥️ ▢ ${library}
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-║ 👥 ▢ *Usuario(s)*
-║ 💫 ▢ ${Object.keys(global.db.data.users).length} 
-║┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
-╚══════════════════⬣`, footer: `${wm}`, 
+*╭┄┄┄┄┄❍${wm}❍┄┄┄┄┄┄╮*
+*┃ 🏵️ ▢ ${ucapan()}*
+*┃➢➢➢➢➢➢➢➢➢➢➢➢*
+*┃ 🔰 ▢ ${taguser}*
+*┃➢➢➢➢➢➢➢➢➢➢➢➢*
+*┃ 🪙 ▢ KANTU-COINS:*
+*┃ 🪙 ▢ ${dorracoins}*
+*┃➢➢➢➢➢➢➢➢➢➢➢➢*
+*┃ ⏱️ ▢ Hora:*
+*┃ ⌚ ▢ ${time}*   
+*┃➢➢➢➢➢➢➢➢➢➢➢➢*
+*┃ ⌛ ▢ Tiempo activo:*
+*┃ ⌛ ▢ ${uptime}*
+*┃➢➢➢➢➢➢➢➢➢➢➢➢*
+*┃ 🛡️ ▢ Versión del bot:*
+*┃ 🛡️ ▢ ${vs}*
+*┃ ☯️ ▢ Librería:*
+*┃ ☯️ ▢ ${library}*
+*┃➢➢➢➢➢➢➢➢➢➢➢➢*
+*┃ 👥 ▢ Usuario(s):*
+*┃ ⚡ ▢ ${Object.keys(global.db.data.users).length}*
+*┃➢➢➢➢➢➢➢➢➢➢➢➢*
+*╰┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄┄╯*`, footer: `*${lenguajeGB['smsPareja']()} ➢ ${pareja ? `${name} 😍 ${conn.getName(pareja)}` : `🙂 ${lenguajeGB['smsResultPareja']()}`}*`, 
 //${name} ${ucapan()}
 
 title: null,
 
-buttonText: "🔰 𝗦𝗘𝗟𝗘𝗖𝗖𝗜𝗢𝗡𝗘 𝗔𝗤𝗨𝗜 🔰", 
+buttonText: "*selección | selection ✅*", 
 
 sections }
 
-await conn.sendMessage(m.chat, listMessage)
+await conn.sendMessage(m.chat, listMessage, {quoted: fkontak})	
 
 }
 
@@ -169,4 +180,3 @@ function ucapan() {
   return res
 
 }
-
